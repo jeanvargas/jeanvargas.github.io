3ebf6136681306026ba772810d29e5abc0779ff6
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-09-14 22:45:57 -0400

    added javascript and splash

---
+++ ARS327/aether/js/myscript.js
@@ -0,0 +1,19 @@
+$.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (
+    ($(window).height() - $(this).outerHeight()) / 2) + 
+     $(window).scrollTop()) + "px"
+  );
+  this.css("left", Math.max(0, (
+    ($(window).width() - $(this).outerWidth()) / 2) + 
+     $(window).scrollLeft()) + "px"
+  );
+  return this;
+}
+
+$("#splash").show();
+$("#splash-content").show().center();
+
+setTimeout(function(){    
+  $("#splash").fadeOut();
+}, 9000);
