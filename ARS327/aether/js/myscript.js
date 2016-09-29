68fc34d3806336eb469445537163bf3718321f4e
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-09-29 01:01:24 -0400

    added favicon to pages

--- ARS327/aether/js/myscript.js
+++ ARS327/aether/js/myscript.js
@@ -1,25 +1,4 @@
-/*$.fn.center = function () {
-  this.css("position","absolute");
-  this.css("top", Math.max(0, (
-    ($(window).height() - $(this).outerHeight()) / 2) + 
-     $(window).scrollTop()) + "px"
-  );
-  this.css("left", Math.max(0, (
-    ($(window).width() - $(this).outerWidth()) / 2) + 
-     $(window).scrollLeft()) + "px"
-  );
-  return this;
-}
-
-$("#splash").show();
-$("#splash-content").show().center();
-
-setTimeout(function(){    
-  $("#splash").fadeOut();
-}, 9000);*/
-
-
- var earth = document.getElementById("earth_sound");
+var earth = document.getElementById("earth_sound");
      function playAudioEarth() {
            earth.play();
     }
